b49314e2f0d6260aeced777041e742e5ab64e150
Leonid Zolotarev <lzolotarev@magicleap.com>
2019-01-29 11:24:13 -0500

    Add Prism Sample

---
+++ sample-prism/README.md
@@ -0,0 +1,6 @@
+# sample-prism
+MagicScript Prism Sample
+
+---
+
+[Water Bottle Model](https://github.com/KhronosGroup/glTF-Sample-Models/tree/master/2.0/WaterBottle) file is Creative Commons licensed, as found here [![CC0](http://i.creativecommons.org/p/zero/1.0/88x31.png)](http://creativecommons.org/publicdomain/zero/1.0/).
---
+++ sample-prism/script/main.js
@@ -0,0 +1,28 @@
+#!/system/bin/script/mxs
+
+import { LandscapeApp } from "lumin";
+
+class App extends LandscapeApp {
+  onAppStart(initArg) {
+    let prism = this.requestNewPrism([0.2, 0.4, 0.2]);
+
+    let name = "res/WaterBottle.glb";
+    let resource = prism.createModelResourceId(name, 1.0);
+    let model = prism.createModelNode(resource);
+
+    prism.getRootNode().addChild(model);
+    super.onAppStart(initArg);
+  }
+  init() {
+    return 0;
+  }
+  updateLoop(delta) {
+    return true;
+  }
+  eventListener(event) {
+    return true;
+  }
+}
+
+let app = new App(0.016);
+app.run();
