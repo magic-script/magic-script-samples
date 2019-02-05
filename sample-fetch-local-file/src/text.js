946521f8ca5a192cb7abe6e49071f87422e8e264
Leonid Zolotarev <lzolotarev@magicleap.com>
2019-02-05 13:57:56 -0500

    Add Fetch Local File Sample

---
+++ sample-fetch-local-file/src/text.js
@@ -0,0 +1,15 @@
+//
+
+import { ui } from "lumin";
+
+export function makeText(prism) {
+  const { UiText, EclipseLabelType, Alignment, HorizontalAlignment } = ui;
+  let node = UiText.CreateEclipseLabel(
+    prism,
+    "Hello\nMagicScript!",
+    EclipseLabelType.kT5
+  );
+  node.setAlignment(Alignment.CENTER_CENTER);
+  //node.setTextAlignment(HorizontalAlignment.kCenter);
+  return node;
+}
