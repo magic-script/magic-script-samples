fe01f59785729c0798ccec28d95d0cc1c50c6f05
Tim Caswell <tcaswell@magicleap.com>
2019-02-27 18:11:41 -0500

    Update sample-promise to new template

--- sample-promise/src/main.js
+++ sample-promise/src/main.js
@@ -1,57 +1,8 @@
+// Add support for things like setTimeout, setInterval and fetch.
+import 'magic-script-polyfills';
 
-import { LandscapeApp, ui } from "lumin";
+// Import our main app
+import { App } from './App.js';
 
-import { setTimeout, setInterval, fetch } from "./builtins.js";
-
-const WORK_DURATION = 3000;
-
-class App extends LandscapeApp {
-  init() {
-    const INTIAL_VALUE = 1;
-    const MAX_VALUE = 1000;
-
-    const prism = this.requestNewPrism([1.0, 0.5, 0.5]);
-
-    // Label
-    const label = ui.UiText.CreateEclipseLabel(prism, "Idle", ui.EclipseLabelType.kT3);
-    label.setAlignment(ui.Alignment.CENTER_CENTER);
-    prism.getRootNode().addChild(label);
-
-    // Button
-    let counter = INTIAL_VALUE;
-    const button = ui.UiButton.Create(prism, 'Request Work');
-    button.setLocalPosition([0.0, -0.125, 0.125]);
-    button.onActivateSub((event) => {
-
-      if (counter <= MAX_VALUE) {
-        counter++;
-      } else {
-        counter = INTIAL_VALUE;
-      }
-
-      this.doWork(counter)
-        .then(value => label.setText(`Request ${value.request} completed in ${value.duration} ms`))
-        .catch(error => label.setText(`Error: ${error}`));
-
-    });
-
-    prism.getRootNode().addChild(button);
-    return 0;
-  }
-  updateLoop(delta) {
-    return true;
-  }
-  eventListener(event) {
-    return true;
-  }
-
-  doWork(count) {
-    const startTime = Date.now();
-    return new Promise((resolve, reject) =>
-      setTimeout(() => resolve({request: count, duration: Date.now() - startTime}), WORK_DURATION)
-    );
-  }
-}
-
-let app = new App(0.016);
-app.run();
+// Launch our app!
+new App(0.5);
