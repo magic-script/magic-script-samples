606949a9c2d7741dc3fdf8b3a89f5722b0e6ee12
Konrad Piascik <kpiascik@magicleap.com>
2019-05-22 10:49:03 -0400

    Add sample app for components

---
+++ sample-components/README.md
@@ -0,0 +1,2 @@
+# mxs-jsx-demo
+Demo app using magic-script-components
---
+++ sample-components/rollup.config.js
@@ -0,0 +1,19 @@
+// Rollup config for consuming some npm modules in MagicScript
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
+export default {
+  external: ['uv', 'lumin'],
+  input: 'src/main.js',
+  preserveModules: true,
+  output: {
+    dir: 'bin',
+    format: 'es'
+  },
+  plugins: [
+    babel({ exclude: "node_modules/**" }),
+    resolve(),
+    commonjs()
+  ]
+};
