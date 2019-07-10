9e40aeaba794a43adee55f5eaed1ff027a594906
Tony Cuadra <tcuadra@magicleap.com>
2019-07-10 12:42:12 -0400

    Initial commit of tic-tac-toe sample
    This is adapted from the ReactJS tutorial: https://reactjs.org/tutorial/tutorial.html
    
    To ease comparison (and show exactly what had to be changed to port to Magic Script),
    this commit does not alter the ReactJS code at all. This means it will not build as is.
    The next commit will contain only the changes required for porting to Magic Script / Lumin.

---
+++ sample-components-tictactoe/rollup.config.js
@@ -0,0 +1,20 @@
+// Rollup config for consuming some npm modules in MagicScript
+
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
+export default {
+  external: ['uv', 'lumin', 'ssl'],
+  input: 'src/main.js',
+  preserveModules: true,
+  output: {
+    dir: 'bin',
+    format: 'es'
+  },
+  plugins: [
+    babel({ exclude: "node_modules/**" }),
+    resolve(),
+    commonjs()
+  ]
+};
