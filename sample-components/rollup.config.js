db4810fdd1cde02992c8efaca34a714ad92f3c67
Tim Caswell <tcaswell@magicleap.com>
2019-08-29 15:29:50 -0500

    Don't include React in bundle.js

--- sample-components/rollup.config.js
+++ sample-components/rollup.config.js
@@ -1,34 +1,37 @@
 // Rollup config for consuming some npm modules in MagicScript
+
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 
 const common = {
-  external: ['uv', 'lumin', 'ssl', 'jpeg', 'png', 'gl'],
   plugins: [
     babel({ exclude: 'node_modules/**' }),
     resolve(),
-    commonjs(),
+    commonjs()
   ]
 };
 
 export default [
   {
     ...common,
+    external: ['uv', 'lumin', 'ssl', 'jpeg', 'png', 'gl'],
     input: 'src/main.js',
     preserveModules: true,
     output: {
       dir: 'bin',
-      format: 'es',
-    },
+      format: 'es'
+    }
   },
   {
     ...common,
+    external: ['react'],
     input: 'src/app.js',
     output: {
+      globals: { react: 'React' },
       file: 'bin/bundle.js',
       format: 'iife',
-      name: 'mxs_bundle',
-    },
-  },
+      name: '_'
+    }
+  }
 ];
